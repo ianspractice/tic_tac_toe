7c4df39ce3de069249015ce4aa763efa2d32b8f9
ianspractice <bodybody.ian@gmail.com>
2025-06-07 10:11:53 -0600

    Replace choice on eval board

--- test.rb
+++ test.rb
@@ -4,16 +4,14 @@ require_relative 'module1'
 def play_game(letter)
   choices = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   display_board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-  win = false
-  until win == true
+  eval_board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
     show_board(display_board)
     puts "What number"
     answer = gets.chomp.to_i
     replace(display_board, answer, letter)
+    replace(eval_board, answer, -1)
     show_board(display_board)
     delete_choice(choices, answer)
-    p choices
-  end
 end
 
 
