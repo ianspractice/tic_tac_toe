c7e0d455133398c1b85aac9791f676bc91578ffe
ianspractice <bodybody.ian@gmail.com>
2025-06-07 15:43:27 -0600

    Change to main file

--- main.rb
+++ main.rb
@@ -1,6 +1,32 @@
 require_relative 'classes/player'
-
-player_x = Player.new('X')
-player_o = Player.new('O')
+require_relative 'classes/board'
 
 
+def play_game
+  choices = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  display_board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  eval_board = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+  player1 = Player.new('X', 1)
+  player2 = Player.new('O', -1)
+  game_board = Board.new
+  win = false
+  game_board.show_board(display_board)
+  until win == true do
+    answer1 = player1.get_choice(choices)
+    game_board.eval_game(display_board, eval_board, choices, answer1, player1.letter, player1.amount)
+    if game_board.evaluate(eval_board, [3, -3])
+      win = true
+      puts "#{player1.letter} wins!"
+      return
+    end
+    answer2 = player2.get_choice(choices)
+    game_board.eval_game(display_board, eval_board, choices, answer2, player2.letter, player2.amount)
+    if game_board.evaluate(eval_board, [3, -3])
+      win = true
+      puts "#{player2.letter} wins!"
+      return
+    end
+  end
+end
+
+play_game
