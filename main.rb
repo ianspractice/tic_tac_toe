e4512dfa5b87fbe5c60d3cc99833be4a8dd7a0d5
ianspractice <bodybody.ian@gmail.com>
2025-06-07 18:54:34 -0600

    Add def for when game is a draw

--- main.rb
+++ main.rb
@@ -9,18 +9,26 @@ def play_game
   player1 = Player.new('X', 1)
   player2 = Player.new('O', -1)
   game_board = Board.new
+  game_plays = 0
   win = false
   game_board.show_board(display_board)
   until win == true do
     answer1 = player1.get_choice(choices)
     game_board.eval_game(display_board, eval_board, choices, answer1, player1.letter, player1.amount)
+    game_plays += 1
     if game_board.evaluate(eval_board, [3, -3])
       win = true
       puts "#{player1.letter} wins!"
       return
     end
+    if game_plays == 9
+      win = true
+      puts "Draw!"
+      return
+    end
     answer2 = player2.get_choice(choices)
     game_board.eval_game(display_board, eval_board, choices, answer2, player2.letter, player2.amount)
+    game_plays += 1
     if game_board.evaluate(eval_board, [3, -3])
       win = true
       puts "#{player2.letter} wins!"
